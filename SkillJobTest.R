d093ee850d6b4409a4f821b4b223249f83c015a8
brickett <brickett@users.noreply.github.com>
2018-07-19 09:49:56 -0500

    Changed state from Iowa to Indiana

--- SkillJobTest.R
+++ SkillJobTest.R
@@ -7,12 +7,12 @@ library(plyr)
 setwd("C:/Users/bjr21/Documents/GitHub/DunnJobSkillMatch")
 
 # Import Data
-EdAtt <- read_sas("IN_IL_EdAtt.sas7bdat")
+EdAtt <- read_sas("IA_IL_EdAtt.sas7bdat")
 Zone <- read_excel("Job_Zones.xlsx")
 Empdata <- read_excel("state_M2017_dl.xlsx")
 
 # Subset to IN and IL
-Empdata <- subset(Empdata, ST == "IN" | ST == "IL")
+Empdata <- subset(Empdata, ST == "IA" | ST == "IL")
 
 ## Workforce Information
 # Recode Education to Job Zone
@@ -28,17 +28,17 @@ EdAtt$JobZone[EdAtt$JobZone == 114 | EdAtt$JobZone == 115 |EdAtt$JobZone == 116]
 
 # Split by state
 EdAtt_IL <- subset(EdAtt, STATEFIP == 17)
-EdAtt_IN <- subset(EdAtt, STATEFIP == 18)
+EdAtt_IA <- subset(EdAtt, STATEFIP == 19)
 
 # Count with Person Weight
 Zone_IL <- count(EdAtt_IL, 'JobZone', wt_var = 'PERWT')
-Zone_IN <- count(EdAtt_IN, 'JobZone', wt_var = 'PERWT')
+Zone_IA <- count(EdAtt_IA, 'JobZone', wt_var = 'PERWT')
 
 Tot_Zone_IL <- sum(Zone_IL$freq)
-Tot_Zone_IN <- sum(Zone_IN$freq)
+Tot_Zone_IA <- sum(Zone_IA$freq)
 
 Zone_IL$Perc <- Zone_IL$freq
-Zone_IN$Perc <- Zone_IN$freq
+Zone_IA$Perc <- Zone_IA$freq
 
 Zone_IL$Perc <- Zone_IL$Perc/Tot_Zone_IL*100
-Zone_IN$Perc <- Zone_IN$Perc/Tot_Zone_IN*100
+Zone_IA$Perc <- Zone_IA$Perc/Tot_Zone_IA*100
